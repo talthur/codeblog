e1b4db40067529447beff21e3614d9a451b7345b
talthur <talthur.ga@gmail.com>
2021-07-20 19:57:07 -0300

    Implementing the view por post list

--- src/main/java/com/spring/codeblog/controller/PostController.java
+++ src/main/java/com/spring/codeblog/controller/PostController.java
@@ -1,10 +1,40 @@
 package com.spring.codeblog.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.codeblog.model.Post;
+import com.spring.codeblog.service.PostService;
 
 @Controller
 public class PostController {
+
+	@Autowired
+	PostService postService;
+
+	@GetMapping(value = "/posts")
+	public ModelAndView getPost() {
+
+		ModelAndView mv = new ModelAndView("posts");
+		List<Post> posts = postService.findAll();
+		mv.addObject("posts", posts);
+		return mv;
+
+	}
 	
-	
+	@GetMapping(value = "/posts/{id}")
+	public ModelAndView getPostDetails(@PathVariable("id") long id) {
+		
+		ModelAndView mv = new ModelAndView("postDetails");
+		Post post = postService.findById(id);
+		mv.addObject("post", post);
+		return mv;
+		
+	}
 
 }
